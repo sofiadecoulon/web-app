450527ea67cfc75792ffe7bb3a2dca92334c8f79
sofia de Coulon <sofia.decoulon@kellogg.northwestern.edu>
2025-02-12 16:52:50 +0000

    html

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # get("/things", :controller => "things", :action => "index")
+  get("/tacos", :controller => "tacos", :action => "index")
+end
+Rails.application.routes.draw do
+  # get("/things", :controller => "things", :action => "index")
+  get("/dice", :controller => "dice", :action => "index")
 end
